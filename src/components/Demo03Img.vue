004e10513434e840715247d078f34049d1a8ecb9
cgp <3549182324@qq.com>
2025-09-09 10:34:59 +0800

    基础：滤镜、渐变、文本、对象

--- src/components/Demo03Img.vue
+++ src/components/Demo03Img.vue
@@ -1,7 +1,7 @@
 <!--
 * @Author: 陈国鹏
 * @Date: 2025/09/02 09:20:30
-* @Description: 画布-动画，颜色，文本，事件
+* @Description: 画布-动画，颜色，文本，事件，动画，图片，滤镜，渐变
 * @Email chengp5@xiaopeng.com
 !-->
 <template>
@@ -20,15 +20,29 @@ let fabricCanvas = null;
 onMounted(() => {
   fabricCanvas = new fabric.Canvas("canvas");
 
-  const text = new fabric.FabricText("hello world", { left: 200, top: 100 });
+  //文本的一些属性
+  const text = new fabric.FabricText("hello world", {
+    left: 200,
+    top: 100,
+    fontFamily: "Comic Sans",
+    fontSize: 40,
+    fontWeight: "bold",
+    linethrough: true,
+    shadow: "green -5px -5px 3px",
+    fontStyle: "italic",
+    fontFamily: "Hoefler Text",
+  });
 
   const rect = new fabric.Rect({
     left: 100,
     top: 100,
     width: 100,
     height: 100,
-    fill: "('000').toHex();",
-    // color: "('fff').toHex(); ",
+    fill: "pink",
+  });
+  //事件可以直接添加在画布的对象上
+  rect.on("selected", function () {
+    console.log("selected a rectangle");
   });
 
   fabricCanvas.add(rect, text);
@@ -38,13 +52,11 @@ onMounted(() => {
     {
       left: 300,
       top: 200,
-      fontSize: 48,
-      fill: "blue",
     },
     {
       duration: 2000, // 设置动画的持续时间为 2000 毫秒
       onChange: fabricCanvas.renderAll.bind(fabricCanvas), // 在动画过程中更新 canvas
-      easing: fabric.util.ease.easeInQuad,
+      easing: fabric.util.ease.easeInQuad, // 设置缓动效果
       onComplete: () => {
         console.log("11111");
       },
@@ -68,7 +80,7 @@ onMounted(() => {
     }
   );
 
-  //灰度滤镜
+  //滤镜
   fabric.FabricImage.fromURL("/lyn4.png")
     .then((img) => {
       img.set({
@@ -83,7 +95,17 @@ onMounted(() => {
       img.scaleToWidth(100);
 
       // 添加滤镜
-      img.filters.push(new fabric.filters.Grayscale());
+      // img.filters.push(new fabric.filters.Grayscale());
+      img.filters.push(
+        // new fabric.filters.Grayscale(),//灰度滤镜
+        // new fabric.filters.Invert(),//反相滤镜
+        new fabric.filters.Blur({ blur: 0.5 }) //模糊滤镜
+        // new fabric.filters.Sepia(),//棕褐色复古滤镜
+        // new fabric.filters.Brightness({ brightness: 10 })//增加亮度
+      );
+
+      // 应用所有滤镜
+      img.applyFilters();
 
       // 应用滤镜
       img.applyFilters();
@@ -95,19 +117,56 @@ onMounted(() => {
       console.error("图片加载失败:", error);
     });
 
+  // 渐变
   var circle = new fabric.Circle({
-    left: 100,
-    top: 100,
+    left: 200,
+    top: 10,
     radius: 50,
   });
-  var gradient = new fabric.Gradient({
-    type: "linear",
-    gradientUnits: "pixels", // or 'percentage'
-    coords: { x1: 0, y1: 0, x2: 0, y2: circle.height },
-    // colorStops:
+  //线性渐变
+  var gradient1 = new fabric.Gradient({
+    type: "linear", // 线性渐变
+    gradientUnits: "pixels", // pixels or pencentage 像素 或者 百分比
+    coords: { x1: 0, y1: 0, x2: circle.width, y2: 0 }, // 至少2个坐标对(x1，y1和x2，y2)将定义渐变在对象上的扩展方式
+    colorStops: [
+      // 定义渐变颜色的数组
+      { offset: 0, color: "red" },
+      { offset: 0.2, color: "pink" },
+      { offset: 0.4, color: "yellow" },
+      { offset: 0.6, color: "green" },
+      { offset: 0.8, color: "blue" },
+      { offset: 1, color: "purple" },
+    ],
   });
-  circle.set("fill", gradient);
+  //径向渐变
+  let gradient2 = new fabric.Gradient({
+    type: "radial",
+    coords: {
+      r1: 50, // 该属性仅径向渐变可用，外圆半径
+      r2: 0, // 该属性仅径向渐变可用，外圆半径
+      x1: 50, // 焦点的x坐标
+      y1: 50, // 焦点的y坐标
+      x2: 50, // 中心点的x坐标
+      y2: 50, // 中心点的y坐标
+    },
+    colorStops: [
+      { offset: 0, color: "#fee140" },
+      { offset: 1, color: "#fa709a" },
+    ],
+  });
+
+  // 设置填充为渐变
+  circle.set("fill", gradient2);
+
   fabricCanvas.add(circle);
+
+  //事件
+  //鼠标级别
+  fabricCanvas.on("mouse:down", function (options) {
+    if (options.target) {
+      console.log("an object was clicked! ", options.target.type); //an object was clicked!  rect
+    }
+  });
 });
 </script>
 
